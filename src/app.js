36f40301d04f3718f66525155c70f3cf76a302ed
keerthikirankuchenpalli-hub <keerthikiran.kuchenpalli@bhrish.com>
2025-11-04 18:11:53 +0530

    made some tests

--- src/app.js
+++ src/app.js
@@ -9,6 +9,17 @@ app.use("/test", (req, res) => {
     res.send('in front there is crocodile festival');
 });
 
+app.use("/final", (req, res) => {
+    res.send('it is second test route');
+});
+
+app.use("/", (req, res) => {
+    res.send('finally done with express js');
+});
+
+
+
+
 app.listen(7272, () => {
     console.log('Server is running on port 7272');
 })
