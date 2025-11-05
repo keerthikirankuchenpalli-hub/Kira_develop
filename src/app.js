17603ce47841999b3baf43c0025b3b494e0151f7
keerthikirankuchenpalli-hub <keerthikiran.kuchenpalli@bhrish.com>
2025-11-05 10:38:55 +0530

    Exploring get, post, patch, delet

--- src/app.js
+++ src/app.js
@@ -2,22 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.use("/Hello", (req, res) => {
-    res.send('hello Chepu ra mama! ');
+
+// This will only handle get call to /user
+app.get("/user", (req, res) => {
+    res.send({ FirstName : "Keerthi kiran" , LastName : "kuchenpalli"});
 });
+
+// This will save data to database
+app.post("/user", (req, res) => {
+    res.send("Data sucessfully saved to the database")
+});
+
+// this will update the data
+app.patch("/user" , (req, res) => {
+    res.send("Data sucessfully updated in the database")
+})
+// this will delete the data
+app.delete("/user" , (req, res) => {
+    res.send("Data sucessfully deleted from the database")
+});
+
+// This will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
     res.send('in front there is crocodile festival');
 });
-
-app.use("/final", (req, res) => {
-    res.send('it is second test route');
-});
-
-app.use("/", (req, res) => {
-    res.send('finally done with express js');
-});
-
-
 
 
 app.listen(7272, () => {
