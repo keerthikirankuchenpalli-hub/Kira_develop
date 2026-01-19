9c5fbf0fb834741f7f98eeb78e30c950b106fb09
keerthikirankuchenpalli-hub <keerthikiran.kuchenpalli@bhrish.com>
2026-01-19 10:27:05 +0530

    mongo db password change

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://kuchenkiran_db_user:qjTqSdu9SuUKcgUH@cluster0.jqws8au.mongodb.net/kiraDev");
+    await mongoose.connect("mongodb+srv://kuchenkiran_db_user:l266H3HEo3GC6Jzv@cluster0.jqws8au.mongodb.net/kiraDev");
 };
 
 module.exports = connectDB;
